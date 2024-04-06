e69aa2e6036e8d75a40edc33166354323d411943
Mehmet Denizli <154231505+foriinji@users.noreply.github.com>
2024-04-06 15:25:25 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "v5.33.0"
+      version = "5.44.0"
     }
   }
 }
+
 provider "aws" {
   region = "us-east-1"
 }
